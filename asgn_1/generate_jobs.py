4768ef90ebcc9ba5ea4c7922ac87914528e2c89f
Aarav <ajaaravbvg@gmail.com>
2023-10-01 01:55:43 +0530

    resolved bugs in the task generation

--- asgn_1/generate_jobs.py
+++ asgn_1/generate_jobs.py
@@ -1,5 +1,3 @@
-import libvirt
-
 from gen_rand_matrix import generate_matrix
 import random
 import subprocess
@@ -26,44 +24,34 @@ VMS = {
 MAX_MATRIX_SIZE = 100
 NUMBER_OF_JOBS = 15
 
-def assign_job(mat1, mat2):
-    virsh_command = """virsh -c qemu:///system qemu-agent-command newUbuntu '{"execute": "guest-exec", "arguments":{"path" : "/home/vboxuser/exe", "arg" : ["1","1$","2$"], "capture-output":true}}'"""
+def assign_job(matrix_size : int, mat1 : str, mat2 : str, vm : str) -> None:
+    virsh_command = """virsh -c qemu:///system qemu-agent-command %s '{"execute": "guest-exec", "arguments":{"path" : "/home/vboxuser/exe", "arg" : [%s, %s, %s], "capture-output":true}}'""" % (vm, matrix_size, mat1, mat2)
 
-    output  = subprocess.run(virsh_command, stdout=subprocess.PIPE, text=True, shell = True )
+    output = subprocess.run(virsh_command, stdout=subprocess.PIPE, text=True, shell = True)
     stdout_string = output.stdout
 
     out = json.loads(stdout_string)
-    # print(out["return"]["pid"])
 
-    virsh_command = """virsh -c qemu:///system qemu-agent-command newUbuntu \'{"execute": "guest-exec-status", "arguments": {"pid":"""+ str(out["return"]["pid"]) +"""}}\'"""
+    virsh_command = """virsh -c qemu:///system qemu-agent-command %s {"execute": "guest-exec-status", "arguments": {"pid": %s}}""" % (vm, str(out["return"]["pid"]))
 
-    output  = subprocess.run(virsh_command, stdout=subprocess.PIPE, text=True, shell = True )
+    output = subprocess.run(virsh_command, stdout=subprocess.PIPE, text=True, shell = True )
     stdout_string = output.stdout
 
-    out = json.loads(stdout_string)
-    
+    out = json.loads(stdout_string) 
 
     encoded_data = str(out["return"]["out-data"])
-
-    # Decode the Base64 data
     decoded_data = base64.b64decode(encoded_data)
+    string_output = str(decoded_data, 'UTF-8')
 
-    # Convert the decoded data to a string
-    string_output = str(decoded_data)
-
-    # Print the decoded data
     print(string_output)
-
-
 
 
 for i in range(NUMBER_OF_JOBS):
     matrix_size = random.randint(1, MAX_MATRIX_SIZE)
-
 
     mat1 = generate_matrix(matrix_size)
     mat2 = generate_matrix(matrix_size)
 
     for key in VMS:
         if VMS[key]['l-range'] <= matrix_size <= VMS[key]['h-range']:
-            assign_job(mat1, mat2)
+            assign_job(matrix_size, mat1, mat2, VMS[key]['name'])
