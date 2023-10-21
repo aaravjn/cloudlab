06e691f2f31e727a2781a958d15bc2ee3c9c02d2
Aarav <ajaaravbvg@gmail.com>
2023-10-21 17:33:56 +0530

    added constructor functions

--- asgn_2/hsfq.cpp
+++ asgn_2/hsfq.cpp
@@ -1,32 +1,53 @@
+/*
+A Simulation of the Hierarchial SFQ algorithm
+*/
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-class Thread {
-    public:
-        int process_time;
-        int start_tag;
-        int finish_tag;
-        int weight;
-        bool is_thread = 1;
-        Node* parent;
-};
 class Node {
     public:
+        int ID;
         Node* parent;
         vector<void*> children;
         bool is_leaf;
         int start_tag;
         int finish_tag;
         int weight;
-        void* (*selector)(vector<void*>, bool);
-        int (*updater)();
+        void* (*selector)(vector<void*>, bool); // Will schedule one of it's child node
+        void (*updater)(); // Will update the state of the ancestor path of the tree. Eg:- start_tag, finish_tag, virtual_time.
         int virtual_time;
+
+        Node(int id, int Weight, void* (*funcSelector)(vector<void*>, bool), void (*funcUpdater)()) { //  Constructor function of the class Node
+            ID = id;
+            weight = Weight;
+            selector = funcSelector;
+            updater = funcUpdater;
+        }
+};
+class Thread { // A class for the thread object in the hsfq tree
+    public:
+        int ID;
+        int process_time; // remaining time to execute the thread, (in seconds)
+        int start_tag;
+        int finish_tag;
+        int weight;
+        bool is_thread;
+        Node* parent; // a pointer to the parent node
+    
+        Thread(int id, int processTime, int startTag, int Weight) { // constructor function for the class Thread
+            ID = id;
+            process_time = processTime;
+            start_tag = startTag;
+            weight = Weight;
+            parent = NULL;
+            is_thread = 1;
+        }
 };
 
-
-void* scheduler(void* root, bool is_thread) {
+void* scheduler(void* root, bool is_thread) { // The main scheduler function of the algorithm.
     if(is_thread) {
         return root;
     }
@@ -37,7 +58,7 @@ void* scheduler(void* root, bool is_thread) {
 }
 
 
-void* sfq_selector(vector<void*> children, bool is_leaf) {
+void* sfq_selector(vector<void*> children, bool is_leaf) { // Selector function which is specific to each node.
     int min_start_tag = 1e9;
     void* finalChild = NULL;
 
@@ -59,5 +80,6 @@ void* sfq_selector(vector<void*> children, bool is_leaf) {
         }
         ((Node *)finalChild)->parent->virtual_time = min_start_tag;
     }
-}
 
+    return finalChild;
+}
